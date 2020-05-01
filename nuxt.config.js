68b186282fd5210cd811155a8fe32383b3a41a42
Stephen Njau <njaustevedomino@gmail.com>
2020-05-01 20:54:38 +0800

    build(husky): added husky hook to dev workflow

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,17 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
+
     extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
     },
     postcss: {
       plugins: {
