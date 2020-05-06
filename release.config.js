5ea18c95597235dcd0023a37dac31d94dc3a8ba6
Stephen Njau <njaustevedomino@gmail.com>
2020-05-06 18:04:58 +0800

    ci(circle): remove backticks

--- release.config.js
+++ release.config.js
@@ -81,7 +81,7 @@ const prepare = [
     '@semantic-release/git',
     {
       assets: ['package.json', 'docs/CHANGELOG.md'],
-      message: `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
     }
   ]
 ]
